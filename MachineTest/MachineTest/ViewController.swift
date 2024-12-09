d4a44380e01a033d3332bcf4ccb8616ceb974bea
Ashutosh Chourasiya <ashutoshchourasiya1203@gmail.com>
2024-12-09 15:27:43 +0530

    Project Set Up.

---
+++ MachineTest/MachineTest/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  MachineTest
+//
+//  Created by Ashutosh Chourasiya on 09/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Hi Ashutosh")
+    }
+}
+
